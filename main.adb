535ef690675f02c7e6ef04f7aa7cfde4383614b7
Sternenfisch <105643443+RobertBoettcherSF@users.noreply.github.com>
2026-08-20 20:39:31 +0200

    Add main.adb for Fast Cosine Transform library

---
+++ main.adb
@@ -0,0 +1,12 @@
+-- main.adb
+with Ada.Text_IO;
+
+procedure Main is
+begin
+   Ada.Text_IO.Put_Line ("=========================================");
+   Ada.Text_IO.Put_Line (" Fast Cosine Transform (FCT) Library");
+   Ada.Text_IO.Put_Line ("=========================================");
+   Ada.Text_IO.Put_Line ("Library successfully compiled.");
+   Ada.Text_IO.Put_Line ("To execute Verification & Validation suite:");
+   Ada.Text_IO.Put_Line ("  $ make test");
+end Main;
